0ddfca657a56ea69c7aebfbd9a77fa0904115abc
natanOds <natanods4@gmail.com>
2021-01-08 21:42:39 -0300

    adicionado Routes e StepHeaders

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,23 +1,24 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <>
             <div className="home-container">
                 <div className="home-content">
-                    <div className="Home actions">
+                    <div className="Home-actions">
                         <h1 className="home-title">
                             Faça seu pedido <br /> que entregamos<br /> pra você!!!
                 </h1>
-                        <h3 className="home subtitle">
+                        <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos<br />
                 levaremoss na sua porta
                 </h3>
-                        <a href="orders" className="home-btn-order">
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                    </a>
+                    </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
