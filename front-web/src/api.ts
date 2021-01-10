328c9aea7632485265a605d5581d5c27bc4fa060
natanOds <natanods4@gmail.com>
2021-01-10 00:39:34 -0300

    erro proposital

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -12,5 +12,5 @@ export function fetchLocalMapBox(local: string){
 }
 
 export function saveOrder(payload: OrderPayload) {
-    return axios.post(`${API_URL}/orders`, payload);
+    return axios.post(`${API_URL}/orders`, payloa);
 }
